b7cf40cbcfc8384318786cde963ce188efdb87a5
Johnathan2009 <150492417+Johnathan2009@users.noreply.github.com>
2023-11-30 20:15:10 -0500

    Update BackdropStageRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackdropStageRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackdropStageRed.java
@@ -1,93 +1,232 @@
 package org.firstinspires.ftc.teamcode;
+/*
+ */
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous
 
-public class BackdropStageRed extends LinearOpMode{
-    private DcMotor         frontLeftMotor = null;
+/**
+ * Team 9960 Revision 170119.0
+ * This program is the scaffold for autonomous operation.
+ * Designed to push the correct button on both beacons
+ *
+ * This robot uses four VEX Mecanum wheels, each direct driven by Neverest 40 motors.
+ * It is designed as a linear op mode, and uses RUN_TO_POSITION motor operation.
+ *
+ */
+@Autonomous()  // @TeleOp(...) is the other common choice
 
-    private DcMotor         frontRightMotor = null;
+public class BDSR extends LinearOpMode {
 
-    private DcMotor         backRightMotor = null;
+    // Declare Devices
+    DcMotor leftFrontMotor = null;
+    DcMotor rightFrontMotor = null;
+    DcMotor leftRearMotor = null;
+    DcMotor rightRearMotor = null;
 
-    private DcMotor         backLeftMotor = null;
+
+    // drive motor position variables
+    private int lfPos;
+    private int rfPos;
+    private int lrPos;
+    private int rrPos;
+
+    // operational constants
+    private double fast = 0.5; // Limit motor power to this value for Andymark RUN_USING_ENCODER mode
+    private double medium = 0.3; // medium speed
+    private double slow = 0.1; // slow speed
+    private double clicksPerInch = 12.5; // empirically measured
+    private double clicksPerDeg = 3.94; // empirically measured
+    private double lineThreshold = 0.7; // floor should be below this value, line above
+    private double redThreshold = 1.9; // red should be below this value, blue above
+
     @Override
     public void runOpMode() {
+        telemetry.setAutoClear(true);
 
-        // Initialize the drive system variables.
-        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
-        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
-        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
-        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
-        Servo pixelArm = hardwareMap.get(Servo.class, "pixel arm");
 
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Initialize the hardware variables.
+        leftFrontMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        rightFrontMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        leftRearMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        rightRearMotor = hardwareMap.dcMotor.get("backRightMotor");
 
+        // The right motors need reversing
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightRearMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftRearMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set the drive motor run modes:
+        leftFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        if(isStopRequested()) return;
-
-        while(opModeIsActive()) {
-            frontLeftMotor.setPower(0.5);
-            backLeftMotor.setPower(0.5);
-            frontRightMotor.setPower(0.5);
-            backRightMotor.setPower(0.5);
-            sleep(1500);
-            frontLeftMotor.setPower(0);
-            backLeftMotor.setPower(0);
-            frontRightMotor.setPower(0);
-            backRightMotor.setPower(0);
-            sleep(1000);
-            frontLeftMotor.setPower(-0.5);
-            backLeftMotor.setPower(0.5);
-            frontRightMotor.setPower(0.5);
-            backRightMotor.setPower(-0.5);
-            sleep(3500);
-            frontLeftMotor.setPower(0);
-            backLeftMotor.setPower(0);
-            frontRightMotor.setPower(0);
-            backRightMotor.setPower(0);
-            sleep(1000);
-            frontLeftMotor.setPower(-0.5);
-            backLeftMotor.setPower(-0.5);
-            frontRightMotor.setPower(-0.5);
-            backRightMotor.setPower(-0.5);
-            sleep(500);
-            frontLeftMotor.setPower(0);
-            backLeftMotor.setPower(0);
-            frontRightMotor.setPower(0);
-            backRightMotor.setPower(0);
-            sleep(500);
-            frontLeftMotor.setPower(0.5);
-            backLeftMotor.setPower(0.5);
-            frontRightMotor.setPower(-0.5);
-            backRightMotor.setPower(-0.5);
-            sleep(500);
-            frontLeftMotor.setPower(0);
-            backLeftMotor.setPower(0);
-            frontRightMotor.setPower(0);
-            backRightMotor.setPower(0);
-            sleep(250);
-            frontLeftMotor.setPower(1);
-            backLeftMotor.setPower(1);
-            frontRightMotor.setPower(1);
-            backRightMotor.setPower(1);
-            sleep(500);
-            frontLeftMotor.setPower(0);
-            backLeftMotor.setPower(0);
-            frontRightMotor.setPower(0);
-            backRightMotor.setPower(0);
-            pixelArm.setPosition(-1);
-            sleep(1000);
-            pixelArm.setPosition(-0.5);
-            // todo: write your code here
-        }
+        // *****************Dead reckoning list*************
+        // Distances in inches, angles in deg, speed 0.0 to 0.6
+        moveRight(-73, medium);
+        moveForward(73, medium);
+        moveRight(73, medium);
+        moveForward(-73, medium);
+        turnClockwise(90, medium);
+        turnClockwise(-90, medium);
     }
+
+    private void moveForward(int howMuch, double speed) {
+        // howMuch is in inches. A negative howMuch moves backward.
+
+        // fetch motor positions
+        lfPos = leftFrontMotor.getCurrentPosition();
+        rfPos = rightFrontMotor.getCurrentPosition();
+        lrPos = leftRearMotor.getCurrentPosition();
+        rrPos = rightRearMotor.getCurrentPosition();
+
+        // calculate new targets
+        lfPos += howMuch * clicksPerInch;
+        rfPos += howMuch * clicksPerInch;
+        lrPos += howMuch * clicksPerInch;
+        rrPos += howMuch * clicksPerInch;
+
+        // move robot to new position
+        leftFrontMotor.setTargetPosition(lfPos);
+        rightFrontMotor.setTargetPosition(rfPos);
+        leftRearMotor.setTargetPosition(lrPos);
+        rightRearMotor.setTargetPosition(rrPos);
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFrontMotor.setPower(speed);
+        rightFrontMotor.setPower(speed);
+        leftRearMotor.setPower(speed);
+        rightRearMotor.setPower(speed);
+
+        // wait for move to complete
+        while (leftFrontMotor.isBusy() && rightFrontMotor.isBusy() &&
+                leftRearMotor.isBusy() && rightRearMotor.isBusy()) {
+
+            // Display it for the driver.
+            telemetry.addLine("Move Foward");
+            telemetry.addData("Target", "%7d :%7d", lfPos, rfPos, lrPos, rrPos);
+            telemetry.addData("Actual", "%7d :%7d", leftFrontMotor.getCurrentPosition(),
+                    rightFrontMotor.getCurrentPosition(), leftRearMotor.getCurrentPosition(),
+                    rightRearMotor.getCurrentPosition());
+            telemetry.update();
+        }
+
+        // Stop all motion;
+        leftFrontMotor.setPower(0);
+        rightFrontMotor.setPower(0);
+        leftRearMotor.setPower(0);
+        rightRearMotor.setPower(0);
+    }
+
+    private void moveRight(int howMuch, double speed) {
+        // howMuch is in inches. A negative howMuch moves backward.
+
+        // fetch motor positions
+        lfPos = leftFrontMotor.getCurrentPosition();
+        rfPos = rightFrontMotor.getCurrentPosition();
+        lrPos = leftRearMotor.getCurrentPosition();
+        rrPos = rightRearMotor.getCurrentPosition();
+
+        // calculate new targets
+        lfPos += howMuch * clicksPerInch;
+        rfPos -= howMuch * clicksPerInch;
+        lrPos -= howMuch * clicksPerInch;
+        rrPos += howMuch * clicksPerInch;
+
+        // move robot to new position
+        leftFrontMotor.setTargetPosition(lfPos);
+        rightFrontMotor.setTargetPosition(rfPos);
+        leftRearMotor.setTargetPosition(lrPos);
+        rightRearMotor.setTargetPosition(rrPos);
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFrontMotor.setPower(speed);
+        rightFrontMotor.setPower(speed);
+        leftRearMotor.setPower(speed);
+        rightRearMotor.setPower(speed);
+
+        // wait for move to complete
+        while (leftFrontMotor.isBusy() && rightFrontMotor.isBusy() &&
+                leftRearMotor.isBusy() && rightRearMotor.isBusy()) {
+
+            // Display it for the driver.
+            telemetry.addLine("Strafe Right");
+            telemetry.addData("Target", "%7d :%7d", lfPos, rfPos, lrPos, rrPos);
+            telemetry.addData("Actual", "%7d :%7d", leftFrontMotor.getCurrentPosition(),
+                    rightFrontMotor.getCurrentPosition(), leftRearMotor.getCurrentPosition(),
+                    rightRearMotor.getCurrentPosition());
+            telemetry.update();
+        }
+
+        // Stop all motion;
+        leftFrontMotor.setPower(0);
+        rightFrontMotor.setPower(0);
+        leftRearMotor.setPower(0);
+        rightRearMotor.setPower(0);
+
+    }
+
+    private void turnClockwise(int whatAngle, double speed) {
+        // whatAngle is in degrees. A negative whatAngle turns counterclockwise.
+
+        // fetch motor positions
+        lfPos = leftFrontMotor.getCurrentPosition();
+        rfPos = rightFrontMotor.getCurrentPosition();
+        lrPos = leftRearMotor.getCurrentPosition();
+        rrPos = rightRearMotor.getCurrentPosition();
+
+        // calculate new targets
+        lfPos += whatAngle * clicksPerDeg;
+        rfPos -= whatAngle * clicksPerDeg;
+        lrPos += whatAngle * clicksPerDeg;
+        rrPos -= whatAngle * clicksPerDeg;
+
+        // move robot to new position
+        leftFrontMotor.setTargetPosition(lfPos);
+        rightFrontMotor.setTargetPosition(rfPos);
+        leftRearMotor.setTargetPosition(lrPos);
+        rightRearMotor.setTargetPosition(rrPos);
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFrontMotor.setPower(speed);
+        rightFrontMotor.setPower(speed);
+        leftRearMotor.setPower(speed);
+        rightRearMotor.setPower(speed);
+
+        // wait for move to complete
+        while (leftFrontMotor.isBusy() && rightFrontMotor.isBusy() &&
+                leftRearMotor.isBusy() && rightRearMotor.isBusy()) {
+
+            // Display it for the driver.
+            telemetry.addLine("Turn Clockwise");
+            telemetry.addData("Target", "%7d :%7d", lfPos, rfPos, lrPos, rrPos);
+            telemetry.addData("Actual", "%7d :%7d", leftFrontMotor.getCurrentPosition(),
+                    rightFrontMotor.getCurrentPosition(), leftRearMotor.getCurrentPosition(),
+                    rightRearMotor.getCurrentPosition());
+            telemetry.update();
+        }
+
+        // Stop all motion;
+        leftFrontMotor.setPower(0);
+        rightFrontMotor.setPower(0);
+        leftRearMotor.setPower(0);
+        rightRearMotor.setPower(0);
+    }
+
 
 }
