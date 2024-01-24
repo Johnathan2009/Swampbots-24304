1e7082452813005bef83566c4d168f12596e9a30
Johnathan2009 <150492417+Johnathan2009@users.noreply.github.com>
2024-01-23 20:42:02 -0500

    Update TelemetryTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTest.java
@@ -3,14 +3,14 @@ package org.firstinspires.ftc.teamcode;  //Declares the package this class is in
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;  //Imports LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;  //Imports TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor;  //Imports DcMotor
-
+//hi how is your day,
 @TeleOp() //Declares this class as teleOp, allowing it to be shown in the teleOp section of the driver station
 public class TelemetryTest extends LinearOpMode {  //The class of the code.  Extends LinearOpMode, making it a subclass
     DcMotor leftFrontMotor = null;  //Declares front left motor and sets it to null
     DcMotor rightFrontMotor = null;  //Declares front right motor and sets it to null
     DcMotor leftRearMotor = null;  //Declares back left motor and sets it to null
     DcMotor rightRearMotor = null;  //Declares back right motor and sets it to null
-    DcMotor liftMotor = null;  //Declares arm lift motor and sets it to null
+    DcMotor slideMotor = null;  //Declares arm lift motor and sets it to null
     @Override  //Overrides certain code
     public void runOpMode() {  //the main method that is called when you run the code
         telemetry.setAutoClear(true);  //sets it to clear the telemetry after telemetry update
@@ -19,7 +19,7 @@ public class TelemetryTest extends LinearOpMode {  //The class of the code.  Ext
         rightFrontMotor = hardwareMap.dcMotor.get("frontRightMotor");  //Declares front right motor
         leftRearMotor = hardwareMap.dcMotor.get("backLeftMotor");  //Declares back left motor
         rightRearMotor = hardwareMap.dcMotor.get("backRightMotor");  //Declares back right motor
-        liftMotor = hardwareMap.dcMotor.get("liftMotor");  //Declares arm lift motor
+        slideMotor = hardwareMap.dcMotor.get("slideMotor");  //Declares arm slide motor
 
         rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);  //Sets the front right motor to Reverse
         leftFrontMotor.setDirection(DcMotor.Direction.FORWARD);  //Sets the front left motor to Forward
@@ -38,7 +38,7 @@ public class TelemetryTest extends LinearOpMode {  //The class of the code.  Ext
             telemetry.addData("BackRightEncoder", rightRearMotor.getCurrentPosition());  //displays the position of the back right motor
             telemetry.addData("FrontLeftEncoder", leftFrontMotor.getCurrentPosition());  //displays the position of the front left motor
             telemetry.addData("FrontRightEncoder", rightFrontMotor.getCurrentPosition());  //displays the position of the front right motor
-            telemetry.addData("LiftPos:", liftMotor.getCurrentPosition());  //displays the position of the arm lift motor
+            telemetry.addData("LiftPos:", slideMotor.getCurrentPosition());  //displays the position of the arm slide motor
             telemetry.update();  // updates the telemetry each time
         }
     }
